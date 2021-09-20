c560765a323ec7d2198d7422c60f8759d8a82395
Omer Cohen <git@omer.io>
2021-09-20 23:44:39 +0000

    fix
    Signed-off-by: Omer Cohen <git@omer.io>

--- asgi.py
+++ asgi.py
@@ -8,17 +8,19 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 from decouple import config
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+config("DJANGO_SETTINGS_MODULE", default="settings")
+django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 import n0temail.routing
 
-config("DJANGO_SETTINGS_MODULE", default="settings")
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(
             URLRouter(n0temail.routing.websocket_urlpatterns)
         ),
