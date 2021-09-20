2b0a096eeefa03d0b6c2cb28303d68899d75005f
Omer Cohen <git@omer.io>
2021-09-20 21:27:30 +0000

    devcontainer
    Signed-off-by: Omer Cohen <git@omer.io>

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
